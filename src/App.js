24706c2dfd638ad3af224a322968d24df2d32977
singhvikramjeet786 <singhvikramjeet786@gmail.com>
2023-12-26 20:06:12 +0100

    added code for displaying & deleting tasks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
 import Header from './components/Header.js';
 import Tasks from './components/Tasks.js';
+import { useState } from "react"
 
 function App() {
-  const name = "Vikram";
+  // const name = "Vikram";
+  const [taskList, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Buy Furnitures',
+      day: 'Dec 28th at 11am',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Meeting at School',
+      day: 'Jan 28th at 9am',
+      reminder: false,
+    },
+    {
+      id: 3,
+      text: 'Food Shopping',
+      day: 'Dec 30th at 1pm',
+      reminder: true,
+    },
+    {
+      id: 4,
+      text: 'House Cleaning',
+      day: 'Jan 3th at 3pm',
+      reminder: true,
+    }
+  ]);
+  
+  //delete Task function
+  const deleteTask = (id) =>{
+    console.log('Delete',id);
+    setTasks(taskList.filter((task)=>task.id !== id))
+  }
+
   return (
     <div className="container">
-      <Header title="Task Tracker"/>
-      <Tasks/>
+      <Header title="Task Tracker" />
+      {taskList.length > 0 ? <Tasks tasks={taskList} onDelete={deleteTask}/> : <p>No Tasks to Show</p>}
       {/* <Header title={1}/> */}
     </div>
   );
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,35 +1,13 @@
-const taskList = [
-    {
-        id:1,
-        text: 'Buy Furnitures',
-        day:'Dec 28th at 11am',
-        reminder: true,
-    },
-    {
-        id:2,
-        text: 'Meeting at School',
-        day:'Jan 28th at 9am',
-        reminder: false,
-    },
-    {
-        id:3,
-        text: 'Food Shopping',
-        day:'Dec 30th at 1pm',
-        reminder: true,
-    },
-    {
-        id:4,
-        text: 'House Cleaning',
-        day:'Jan 3th at 3pm',
-        reminder: true,
-    }
-]
+import Task from "./Task"
 
-const Tasks = () => {
+const Tasks = ({tasks, onDelete}) => {
+    
+
   return (
+    // setTasks([...taskList, {id:5,text:"VIK",day:"test",reminder:true}])
     <>
-        {taskList.map((task) => (
-        <h3 key={task.id}>{task.text}</h3>
+        {tasks.map((task) => (
+        <Task key={task.id} task={task} onDelete={onDelete}/>
         ))}
     </>
   )
