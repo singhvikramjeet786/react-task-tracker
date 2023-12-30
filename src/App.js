c20e3ca25c819420dba26650ace4c9ba9b0a9e6f
singhvikramjeet786 <singhvikramjeet786@gmail.com>
2023-12-30 20:06:33 +0100

    did CRUD operations on dbjson using local restAPIs

--- src/App.js
+++ src/App.js
@@ -27,26 +27,61 @@ function App() {
     return data
   }
 
-  //Add task
-  const addTask = (task) => {
-    console.log(task)
-    const id = Math.floor(Math.random() * 10000) + 1
+   //fetch single task
+   const fetchTask = async (id) => {
+    //url of our local server db we created using jsonserver
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
+    console.log(data)
+    return data
+  }
 
-    const newTask = { id, ...task }
-    setTasks([...taskList, newTask])
+  //Add task
+  const addTask = async (task) => {
+    console.log(task)
+
+    const res = await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-type':'application/json',
+      },
+      body: JSON.stringify(task),
+    })
+    const data = await res.json()
+    
+    setTasks([...taskList, data])
+
+    // const id = Math.floor(Math.random() * 10000) + 1
+    // const newTask = { id, ...task }
+    // setTasks([...taskList, newTask])
   }
   //delete Task function
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
     console.log('Delete', id);
+    await fetch(`http://localhost:5000/tasks/${id}`,{
+      method: 'DELETE',
+    })
     setTasks(taskList.filter((task) => task.id !== id))
   }
 
   //Toggle reminder for tasks
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id);
+    const updatedTask = {...taskToToggle, reminder: !taskToToggle.reminder}
+    const res = await fetch(`http://localhost:5000/tasks/${id}`,{
+      method:'PUT',
+      headers: {
+        'Content-type':'application/json',
+      },
+      body: JSON.stringify(updatedTask)
+    })
+
+    const data = await res.json()
     console.log("toggle", id);
     setTasks(taskList.map((task) =>
-      task.id === id ? { ...task, reminder: !task.reminder } : task
+      task.id === id ? { ...task, reminder: data.reminder } : task
     ))
+
   }
   return (
     <div className="container">
